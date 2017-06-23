e2fb5faf4fbee6b6f1adc650613f46bc523713ef
16raghu <16raghu@gmail.com>
2017-06-23 14:21:52 -0700

    Using Generics for redirecting

--- website/music/urls.py
+++ website/music/urls.py
@@ -5,13 +5,13 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    #/music/
-    url(r'^$', views.index, name='index'),
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    #/music/712
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /music/712
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/712/fav
-    url(r'^(?P<album_id>[0-9]+)/fav/$', views.fav, name='fav'),
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,51 +1,22 @@
-#from django.http import Http404
-import logging
-from django.shortcuts import render, get_object_or_404
-from . models import  Album,Song
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Album
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    cotext_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
 
-logger = logging.getLogger(__name__)
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-
-    return render(request,'music/index.html',context)
-
-
-def detail(request, album_id):
-    album = get_object_or_404(Album,pk=album_id)
-
-    # try:
-    #   album = Album.objects.get(pk=album_id)
-    # except Album.DoesNotExist:
-    #   raise Http404("Album does not exist")
-    return render(request, 'music/detail.html', {'album': album})
-
-def fav(request, album_id):
-
-    album = get_object_or_404(Album, pk=album_id)
-    print(album)
-    try:
-        print("ho");
-        print("ho:"+request.POST['song'])
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': 'You did not select a valid song',
-        })
-    else:
-        selected_song.is_fav = True
-        print(selected_song)
-        print(selected_song.is_fav)
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
-
-
-
-
-
-
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
 
 
 
